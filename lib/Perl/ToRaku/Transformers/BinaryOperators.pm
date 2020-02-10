2ddfe1fc27cd1d1ba77870c2a0252411a883ae9d
Jeffrey Goff <drforr@pobox.com>
2020-02-10 04:06:23 +0000

    Patch 'new Foo(...)'

--- lib/Perl/ToRaku/Transformers/BinaryOperators.pm
+++ lib/Perl/ToRaku/Transformers/BinaryOperators.pm
@@ -3,10 +3,16 @@ package Perl::ToRaku::Transformers::BinaryOperators;
 use strict;
 use warnings;
 
+# 'new Foo(2)' => 'Foo.new(2)'
+# 'Foo->new(2)' => 'Foo.new(2)'
+# '$x->$y' => '$x.$y' # And so on.
+#
 sub transformer {
   my $self = shift;
   my $obj  = shift;
   my $ppi  = $obj->_ppi;
+
+  _fixup_new( $ppi );
 
   # This may *look* like a transitive loop, but it's not because it's all
   # happening at once.
@@ -26,11 +32,34 @@ sub transformer {
   my $operator_tokens = $ppi->find( 'PPI::Token::Operator' );
   if ( $operator_tokens  ) {
     for my $operator_token ( @{ $operator_tokens } ) {
-#      next unless $operator_token->sprevious_sibling;
       next unless exists $map{ $operator_token->content };
 
       my $new_content = $map{ $operator_token->content };
       $operator_token->set_content( $new_content );
+    }
+  }
+}
+
+sub _fixup_new {
+  my $ppi = shift;
+
+  my $statements = $ppi->find( 'PPI::Statement' );
+  if ( $statements ) {
+    for my $statement ( @{ $statements } ) {
+      next unless $statement->first_element->content eq 'new';
+
+      my $new_new = PPI::Token::Word->new( 'new' );
+      $statement->first_element->snext_sibling->insert_after( $new_new );
+
+      my $new_arrow = PPI::Token::Operator->new( '->' );
+      $statement->first_element->snext_sibling->insert_after( $new_arrow );
+
+      my $first_element = $statement->first_element;
+
+      if ( $first_element->next_sibling->isa( 'PPI::Token::Whitespace' ) ) {
+        $first_element->next_sibling->delete();
+      }
+      $first_element->delete();
     }
   }
 }
--- t/BinaryOperators.t
+++ t/BinaryOperators.t
@@ -6,12 +6,21 @@ use warnings;
 use Test::More;
 use Perl::ToRaku;
 
-plan tests => 15;
+plan tests => 18;
 
 my $package = 'Perl::ToRaku::Transformers::BinaryOperators';
 my $toRaku  = Perl::ToRaku->new;
 
 use_ok $package;
+
+is $toRaku->test_transform( $package, 'new Foo(2)' ),
+   'Foo.new(2)';
+
+is $toRaku->test_transform( $package, 'Foo->new(2)' ),
+   'Foo.new(2)';
+
+is $toRaku->test_transform( $package, 'Foo->newt(2)' ),
+   'Foo.newt(2)';
 
 is $toRaku->test_transform( $package, '$x->$y' ),
    '$x.$y';
